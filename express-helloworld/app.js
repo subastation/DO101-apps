71e775268755bedccbc6fd36ec81f5e4653ed685
Pie <ith.cslsec.10@gmail.com>
2020-06-12 16:13:36 +0200

    V5

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Ready for version 5\n');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Bound 8080/tcp');
 });
 
